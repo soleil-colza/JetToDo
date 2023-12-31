e4ce8b9938bb78721c0d364a016e383fda869122
Hina Nakahira <soleil.colza@icloud.com>
2023-12-31 12:25:40 +0900

    set up the database

---
+++ app/src/main/java/com/example/myapplication/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1) //Databaseアノテーションを付ける, entitiesにはテーブルとして使用するクラスを指定する, versionはデータベースのバージョンを指定する
+abstract class AppDatabase: RoomDatabase() { //抽象クラスとして宣言する
+    abstract fun taskDao(): TaskDao //TaskDaoを返す関数を宣言する
+}
